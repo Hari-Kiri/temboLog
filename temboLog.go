8b9df68db521b97bc252071efe568eac830c403d
@Hari-Kiri <pratamahari89@gmail.com>
2022-09-27 12:21:07 +0700

    Fix fatal logging to print error. Delete log shortfile.

--- temboLog.go
+++ temboLog.go
@@ -8,20 +8,20 @@ import (
 // Fatal message will print Your provided message as error log then stop the application.
 func FatalLogging(message string, error any) {
 	fatalLog := log.New(os.Stderr, "[FATAL] ", 1)
-	fatalLog.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	fatalLog.Fatalln(message, message)
+	fatalLog.SetFlags(log.Ldate | log.Ltime)
+	fatalLog.Fatalln(message, error)
 }
 
 // Error message will print Your provided error message
 func ErrorLogging(message string, error any) {
 	errorLog := log.New(os.Stderr, "[ERROR] ", 1)
-	errorLog.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	errorLog.SetFlags(log.Ldate | log.Ltime)
 	errorLog.Println(message, error)
 }
 
 // Info message will print Your standard provided message
 func InfoLogging(message ...any) {
-	infoLog := log.New(os.Stdout, "[INFO]", 0)
-	infoLog.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	infoLog := log.New(os.Stdout, "[INFO] ", 0)
+	infoLog.SetFlags(log.Ldate | log.Ltime)
 	infoLog.Println(message...)
 }
